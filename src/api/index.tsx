6e53be68d734a1dd2dd5a6243a0275f43ba08634
= <=>
2023-08-11 12:28:23 +0800

    switch to HTTPS

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: 'http://birtracker.nat911.com/api/graphql',
+    uri: 'https://birtracker.nat911.com/api/graphql',
     cache: new InMemoryCache(),
 });
 
