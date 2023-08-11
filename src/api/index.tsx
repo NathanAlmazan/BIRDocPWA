f7d6f9a599bd2da82b6208ad973639505df2fd60
= <=>
2023-08-11 11:52:59 +0800

    added build folder

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: 'http://localhost:8080/',
+    uri: 'http://birtracker.nat911.com/api/graphql',
     cache: new InMemoryCache(),
 });
 
